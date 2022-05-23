762ad0f42c45478e78ea33e123d81e43f887d80d
pedro <pedrohperoni@gmail.com>
2022-05-23 14:34:56 -0300

    fix: start script

--- src/routers/API-Router.ts
+++ src/routers/API-Router.ts
@@ -3,18 +3,21 @@ import * as clarifaiController from "../controllers/clarifaiController.js";
 import * as unsplashController from "../controllers/unsplashController.js";
 import { schemaValidationMiddleware } from "../middlewares/schemaValidationMiddleware.js";
 import foodURLschema from "../schemas/foodURLSchema.js";
+import { validateJWT } from "../middlewares/JWTValidationMiddleware.js";
 
 
 const API_Router = Router();
 
 API_Router.post(
   "/clarifai/food",
+  validateJWT,
   schemaValidationMiddleware(foodURLschema),
   clarifaiController.getFoodDescriptionAndTags
 );
 
 API_Router.post(
   "/unsplash/:image",
+  validateJWT,
   unsplashController.getFirstImageResultByName
 );
 export default API_Router;
--- src/routers/postRouter.ts
+++ src/routers/postRouter.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import * as postController from "../controllers/postController.js";
 import { schemaValidationMiddleware } from "../middlewares/schemaValidationMiddleware.js";
 import createPostSchema from "../schemas/createPostSchema.js";
+import { validateJWT } from "../middlewares/JWTValidationMiddleware.js";
 
 const postRouter = Router();
 
 postRouter.post(
   "/posts/create",
+  validateJWT,
   schemaValidationMiddleware(createPostSchema),
   postController.createPost
 );
